04f4576c3f6cab55276dac1f3356d0c00b440f97
sakshijain032 <46872163+sakshijain032@users.noreply.github.com>
2019-03-17 00:38:11 +0530

    Add files via upload
    I'm sure you will like this code

---
+++ basic_calculator_in_one_step.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 00:23:08 2019
+
+@author: Sakshi
+"""
+
+#eval :- It evaluate result for following expression
+print(eval(input("enter any calculation you want me to calculate")))
